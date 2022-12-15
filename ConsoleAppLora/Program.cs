337bdf6b87521673be537ac4a5da54738648045d
Mikael Olsson <zangiloxd@hotmail.com>
2022-12-15 19:51:25 +0100

    First part done?

--- ConsoleAppLora/Program.cs
+++ ConsoleAppLora/Program.cs
@@ -19,13 +19,13 @@ namespace ConsoleApp.Lora
         {
             Console.WriteLine("\nALoRa ConsoleApp - A The Things Network C# Library\n");
 
-            string TTN_APP_ID = "miol00-test-device@ttn";
-            string TTN_ACCESS_KEY = "NNSXS.4UC7MI7DRMQOXTFJJOCO42M7UV4ZCUGVAVGO6ZY.O7LMGTORRNONL4EBPAT7OS7MWZ2WRDRZQTN5D6PQEBJEO6J6HUNQ";
-            string TTN_REGION = "eu1";
-
-            //string TTN_APP_ID = "campusborlangeelsys";
-            //string TTN_ACCESS_KEY = "NNSXS.GTNTSDWU4ZBW365PKTHWGE4KL67KY75ZHVKCMZA.AC2JT7W3WIRZ3PVLQQXCAYFUTCWM5426VBVSAB7OC4GDBIC5SBQQ";
+            //string TTN_APP_ID = "miol00-test-device@ttn";
+            //string TTN_ACCESS_KEY = "NNSXS.4UC7MI7DRMQOXTFJJOCO42M7UV4ZCUGVAVGO6ZY.O7LMGTORRNONL4EBPAT7OS7MWZ2WRDRZQTN5D6PQEBJEO6J6HUNQ";
             //string TTN_REGION = "eu1";
+
+            string TTN_APP_ID = "campusborlangeelsys";
+            string TTN_ACCESS_KEY = "NNSXS.GTNTSDWU4ZBW365PKTHWGE4KL67KY75ZHVKCMZA.AC2JT7W3WIRZ3PVLQQXCAYFUTCWM5426VBVSAB7OC4GDBIC5SBQQ";
+            string TTN_REGION = "eu1";
 
             //string TOPICS = "v3/+/devices/+/up";
 
@@ -53,17 +53,26 @@ namespace ConsoleApp.Lora
         private static void App_MessageReceived(TTNMessage obj)
         {
             var data = obj.Payload != null ? BitConverter.ToString(obj.Payload) : string.Empty;
+            var deviceId = obj.DeviceID;
             Console.WriteLine($"Message Timestamp: {obj.Timestamp}, Device: {obj.DeviceID}, Topic: {obj.Topic}, Payload: {data}");
 
 
             var cleanData = Elsys.Decoder.Decoder.Decode(data);
 
-
+            DisplayMessage(deviceId,cleanData);
         }
 
-        public static void (){
+        public static void DisplayMessage(string deviceId,Data cleanData){
+            if(deviceId.EndsWith("a4") || deviceId.EndsWith("a5"))
+            {
+                Console.WriteLine($"Decoded messages: Co2 {cleanData.Co2}, Humidity {cleanData.Humidity}, Light {cleanData.Light}, Motion {cleanData.Motion}, Temperature {cleanData.Temperature}, Vdd {cleanData.Vdd}");
+            }
 
-}
+            if (deviceId.EndsWith("fd") || deviceId.EndsWith("fe"))
+            {
+                Console.WriteLine($"Decoded messages: AccMotion {cleanData.AccMotion}, ExternalTemperature {cleanData.ExternalTemperature}, Humidity {cleanData.Humidity}, Pressure {cleanData.Pressure}, Temperature {cleanData.Temperature}, Vdd {cleanData.Vdd}, X {cleanData.X}, Y {cleanData.Y}, Z {cleanData.Z}");
+            }
+        }
 
 
 
