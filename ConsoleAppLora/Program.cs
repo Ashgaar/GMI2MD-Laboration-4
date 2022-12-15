5e409409d225dcd757fb554a2033fd7eb6dcfd69
Mikael Olsson <zangiloxd@hotmail.com>
2022-12-15 20:14:57 +0100

    dockerized

--- ConsoleAppLora/Program.cs
+++ ConsoleAppLora/Program.cs
@@ -19,15 +19,9 @@ namespace ConsoleApp.Lora
         {
             Console.WriteLine("\nALoRa ConsoleApp - A The Things Network C# Library\n");
 
-            //string TTN_APP_ID = "miol00-test-device@ttn";
-            //string TTN_ACCESS_KEY = "NNSXS.4UC7MI7DRMQOXTFJJOCO42M7UV4ZCUGVAVGO6ZY.O7LMGTORRNONL4EBPAT7OS7MWZ2WRDRZQTN5D6PQEBJEO6J6HUNQ";
-            //string TTN_REGION = "eu1";
-
             string TTN_APP_ID = "campusborlangeelsys";
             string TTN_ACCESS_KEY = "NNSXS.GTNTSDWU4ZBW365PKTHWGE4KL67KY75ZHVKCMZA.AC2JT7W3WIRZ3PVLQQXCAYFUTCWM5426VBVSAB7OC4GDBIC5SBQQ";
             string TTN_REGION = "eu1";
-
-            //string TOPICS = "v3/+/devices/+/up";
 
 
             using (var app = new TTNApplication(TTN_APP_ID, TTN_ACCESS_KEY, TTN_REGION))
